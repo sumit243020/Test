3446dface599b882dc5400018c0508a8ca76432f
sumit243020 <121555429+sumit243020@users.noreply.github.com>
2023-02-17 15:02:39 +0530

    role crud changes

--- UAMService/src/main/java/com/mango/uam/controller/UamRoleController.java
+++ UAMService/src/main/java/com/mango/uam/controller/UamRoleController.java
@@ -1,10 +1,15 @@
 package com.mango.uam.controller;
 
+import java.net.http.HttpHeaders;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mango.uam.Service.UamRoleService;
 import com.mango.uam.entity.UamRoleEntity;
+
+import ch.qos.logback.core.status.Status;
 
 @RestController
 @RequestMapping("/role")
@@ -21,10 +28,28 @@ public class UamRoleController {
 	UamRoleService uamRoleService;
 		
 	@GetMapping("/getAllRole")
-	public List<UamRoleEntity> getAllRoles() {
+	public ResponseEntity<?>  getAllRoleDetails() {
 		
-		return uamRoleService.getAllRoles();
-
+		Map<String , Object> map = new HashMap<String ,Object>();
+		
+		List<UamRoleEntity> uamRoleEntity = uamRoleService.getAllRoles();
+		
+		if(!uamRoleEntity.isEmpty()) {
+			
+			map.put("status", 1);
+			
+			map.put("data", uamRoleEntity);
+			
+			 return new ResponseEntity<>(map,HttpStatus.OK);
+		} else {
+			map.clear();
+			map.put("status",0);
+			
+			map.put("Message","Data is not found");
+			
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
 	}
 	
 	
